3ddc83d97dc5a4c19b80d2d10190b23a9af9e0cb
alex vydrin <tapat@mail.ru>
2020-03-22 23:04:32 +0300

    first commit

---
+++ product/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Product(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    passport_link = models.CharField(max_length=200)
+    site_link = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
