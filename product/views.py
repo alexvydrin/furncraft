d8701c09ffa6327b35c7e2a436387e2e3135700a
alex vydrin <tapat@mail.ru>
2020-03-24 17:08:10 +0300

    20200324_02

--- product/views.py
+++ product/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Product
+
 
 def product_list(request):
-    return render(request, 'product/product_list.html', {})
+    # products = Product.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    products = Product.objects.order_by('published_date')
+    return render(request, 'product/product_list.html', {'products': products})
