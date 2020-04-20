e9028a4a5d56ac09f1e7ece9079ba0a20f03d276
alex vydrin <tapat@mail.ru>
2020-04-20 22:11:31 +0300

    20200420_01_media

--- main/urls.py
+++ main/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
-# import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
 ]
 
-# urlpatterns += static('/data/', document_root=os.path.join(settings.BASE_DIR, 'data')) - не работает
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
